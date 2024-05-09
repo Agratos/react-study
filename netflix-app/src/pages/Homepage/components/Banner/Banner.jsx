4cfb79acdc735c451d79a6fad953a4e642089a3a
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2024-05-09 17:03:14 +0900

    no message

--- netflix-app/src/pages/Homepage/components/Banner/Banner.jsx
+++ netflix-app/src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,36 +1,40 @@
-import React from "react"
+import React from 'react';
 import Alert from 'react-bootstrap/Alert';
 
-import { usePopularMoviesQuery } from "../../../../hooks/apis/usePopularMovies"
-import platformStore from "../../../../store/platformStore";
+import { usePopularMoviesQuery } from '../../../../hooks/apis/usePopularMovies';
+import platformStore from '../../../../store/platformStore';
 
-import "./Banner.style.css";
+import './Banner.style.css';
 
 const Banner = () => {
-    const { data , isLoading, isError, error } = usePopularMoviesQuery();
-    const { platform }  = platformStore((state) => state)
+	const { data, isLoading, isError, error } = usePopularMoviesQuery();
+	const { platform } = platformStore((state) => state);
 
-    if(isLoading) {
-        <h1>Loading...</h1>
-    }
+	if (isLoading) {
+		return <h1>Loading...</h1>;
+	}
 
-    if(isError) {
-        <Alert variant="danger">{error.message}</Alert>
-    }
-    
-    return (
-        <div style={!isLoading ? {
-            backgroundImage:
-                `url(${process.env.REACT_APP_BACKGROUND_URL}${data?.results[0].poster_path})`
-            } : {}}
-            className="banner"
-        >
-            <div className="text-white banner-text-area">
-                <h1>{data?.results[0].title}</h1>
-                <p>{data?.results[0].overview}</p>
-            </div>
-        </div>
-    )
-}
+	if (isError) {
+		return <Alert variant='danger'>{error.message}</Alert>;
+	}
 
-export default Banner
+	return (
+		<div
+			style={
+				!isLoading
+					? {
+							backgroundImage: `url(${process.env.REACT_APP_BACKGROUND_URL}${data?.results[0].poster_path})`,
+					  }
+					: {}
+			}
+			className='banner'
+		>
+			<div className='text-white banner-text-area'>
+				<h1>{data?.results[0].title}</h1>
+				<p>{data?.results[0].overview}</p>
+			</div>
+		</div>
+	);
+};
+
+export default Banner;
